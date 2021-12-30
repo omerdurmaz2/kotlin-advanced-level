4042ee3d84e9add3f00b0c8f91832ed0bf7cdef7
omerdurmaz <omer.durmaz@appcent.mobi>
2021-12-31 01:09:44 +0300

    * use nav graph and send arguments between fragments

---
+++ level1_jetpack_navigation/app/src/main/java/com/android/jetpacknavigation/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.android.jetpacknavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_fragment_first).setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(50)
+            Navigation.findNavController(it)
+                .navigate(action)
+        }
+    }
+
+}
